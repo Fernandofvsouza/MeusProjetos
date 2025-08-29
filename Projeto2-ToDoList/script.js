8cdfa07f670533aa5ea418f2427be9c3f2d12012
Fernando Souza <fernandofvsouza@icloud.com>
2025-08-29 12:35:38 +0100

    Minha lista de tarefas

---
+++ Projeto2-ToDoList/script.js
@@ -0,0 +1,60 @@
+const localStorageKey = 'listaTarefas'
+
+function newTask(){
+    let input = document.getElementById('input')
+    
+
+    if(!input.value){
+        alert('Digite uma tarefa')
+    }else if(validateIfExists()){
+        alert('Essa tarefa já está em sua lista')
+    }
+    else{
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
+        
+        values.push({
+            name: input.value
+        })
+
+        localStorage.setItem(localStorageKey, JSON.stringify(values))
+
+        showItens()
+    }
+    input.value = ''
+}
+
+function showItens(){
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
+    let list = document.getElementById('list')
+    list.innerHTML = ''
+
+    for(let i=0;i < values.length;i++){
+        list.innerHTML += `<li>${values[i]['name']}<button id= 'btn-ok' onclick='removeItens("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-all" viewBox="0 0 16 16">
+        <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z"/>
+        </svg>  </button> </li>`
+    }
+}
+
+function removeItens(data){
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
+    let index = values.findIndex(x=> x.name == data)
+    values.splice(index, 1)
+    localStorage.setItem(localStorageKey, JSON.stringify(values))
+    showItens()
+}
+
+function validateIfExists(){
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
+    let inputValue = document.getElementById('input').value
+    let exists = values.findIndex(x=> x.name == inputValue)
+
+    if (exists !== -1) {
+        // já existe
+        return true
+    } else {
+        // não existe
+        return false
+    }
+}
+
+showItens()
